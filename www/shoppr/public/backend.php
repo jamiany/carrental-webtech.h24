e877e07346b8f23360f36c33710bc25c0bdbc14f
jamian.rajakone <jamian2011@gmail.com>
2024-12-12 22:21:48 +0100

    load w3css and fetch data from backend

--- www/shoppr/public/backend.php
+++ www/shoppr/public/backend.php
@@ -1,14 +1,23 @@
 <?php
+    header('Content-type: application/json');
     $method = $_SERVER['REQUEST_METHOD'];
     $body = file_get_contents("php://input");
     parse_str($_SERVER['QUERY_STRING'], $params);
 
     if($method == 'GET'){
         if($params['operation'] == 'list'){
-            $person = [ 'name' => 'Muster',
-                'vorname' => 'Hans',
-                'adresse' => 'Musterstr. 23' ];
-                $personJSON = json_encode($person);
+            $person = [
+                [ 
+                    'name' => 'Volkswagen',
+                    'modell' => 'Kombi'
+                ],
+                [ 
+                    'name' => 'Skoda',
+                    'modell' => 'Octavia'
+                ]
+            ];
+
+            $personJSON = json_encode($person);
             echo($personJSON);
             return;
         }
--- www/shoppr/public/js/main.js
+++ www/shoppr/public/js/main.js
@@ -1,10 +1,30 @@
 const { createApp, ref } = Vue
 
 createApp({
-setup() {
-    const message = ref('Hello vue!')
-    return {
-    message
+    data: function() {
+        return {
+            carList: null
+        }
+    },
+    methods: {
+        myFunction() {
+            var x = document.getElementById("demo");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+            }
+        }
+    },
+    created: function() {
+        fetch('/backend.php?operation=list')
+            .then(r => r.json())
+            .then(l => this.carList = l)
+    },
+    setup() {
+        const message = ref('Hello vue!')
+        return {
+            message
+        }
     }
-}
 }).mount('#app')
